1a7c30216a464be90bedbd1e7d51eaa7840e656b
Hiranur İleyna Bilir <hiranurbilir0@gmail.com>
2026-02-10 22:49:42 +0300

    Project improved.

--- src/Main.java
+++ src/Main.java
@@ -20,6 +20,8 @@ public class Main {
             int choice = scanner.nextInt();
             scanner.nextLine();
 
+            Company hospital = new Company("Özel Hastane");
+
             switch (choice) {
                 case 1:
                     System.out.print("Kullanıcı adı: ");
@@ -32,7 +34,16 @@ public class Main {
 
                     if (loggedIn != null) {
                         System.out.println("Hoş geldiniz, " + loggedIn.getUsername());
-                        // ŞİMDİLİK BURADA DURUYORUZ
+
+                        if (loggedIn instanceof Admin) {
+                            adminMenu();
+                        }
+                        else if (loggedIn instanceof Customer) {
+                            customerMenu((Customer) loggedIn, scanner);
+
+                        } else if (loggedIn instanceof RegularUser) {
+                            userMenu((RegularUser) loggedIn, hospital, scanner);
+                        }
                     } else {
                         System.out.println("Hatalı giriş!");
                     }
@@ -65,4 +76,102 @@ public class Main {
 
         return authService;
     }
+    private static void adminMenu() {
+        System.out.println("\n--- ADMIN MENÜ ---");
+        System.out.println("1- Şirket ekle");
+        System.out.println("2- Kullanıcı listele");
+        System.out.println("0- Çıkış");
+    }
+    private static void customerMenu(
+            Customer customer,
+            Scanner scanner) {
+
+        boolean inMenu = true;
+
+        while (inMenu) {
+            System.out.println("\n--- CUSTOMER MENÜ ---");
+            System.out.println("1- Uygun saat ekle");
+            System.out.println("0- Çıkış");
+            System.out.print("Seçim: ");
+
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (choice) {
+                case 1:
+                    System.out.print("Saat (örn: 10): ");
+                    int hour = scanner.nextInt();
+                    scanner.nextLine();
+
+                    TimeSlot slot = new TimeSlot(
+                            LocalDate.now(),
+                            LocalTime.of(hour, 0)
+                    );
+
+                    customer.getCompany().addAvailableSlot(slot);
+                    System.out.println("Saat eklendi!");
+                    break;
+
+                case 0:
+                    inMenu = false;
+                    break;
+            }
+        }
+    }
+
+    private static void userMenu(
+            RegularUser user,
+            Company company,
+            Scanner scanner) {
+
+        AppointmentService appointmentService = new AppointmentService();
+        boolean inMenu = true;
+
+        while (inMenu) {
+            System.out.println("\n--- KULLANICI MENÜ ---");
+            System.out.println("1- Randevu al");
+            System.out.println("0- Çıkış");
+            System.out.print("Seçim: ");
+
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (choice) {
+                case 1:
+                    System.out.println("\nUygun Saatler:");
+
+                    for (int i = 0; i < company.getAvailableSlots().size(); i++) {
+                        TimeSlot slot = company.getAvailableSlots().get(i);
+                        if (slot.isAvailable()) {
+                            System.out.println(i + " - " + slot);
+                        }
+                    }
+
+                    System.out.print("Saat seç (index): ");
+                    int index = scanner.nextInt();
+                    scanner.nextLine();
+
+                    Appointment appointment =
+                            appointmentService.createAppointment(
+                                    user,
+                                    company,
+                                    company.getAvailableSlots().get(index)
+                            );
+
+                    if (appointment != null) {
+                        System.out.println("Randevu oluşturuldu!");
+                    } else {
+                        System.out.println("Saat dolu!");
+                    }
+                    break;
+
+                case 0:
+                    inMenu = false;
+                    break;
+            }
+        }
+    }
+
+
+
 }
